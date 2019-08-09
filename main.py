35f741fcfcbbdd78fad49e04d8e4938b488ba53c
Erik Kamph <erikkamph38@gmail.com>
2019-08-09 22:35:29 +0200

    Second Commit

--- main.py
+++ main.py
@@ -3,6 +3,11 @@ import os
 import fileinput
 import sys
 import argparse
+from subprocess import call
+
+
+def clear():
+    _ = call("clear" if os.name == "posix" else "cls")
 
 
 def containing(search, file):
@@ -41,41 +46,50 @@ def preview(themes_location):
 
 
 def usage():
-    parser = argparse.ArgumentParser(description="Preview Xresource theme files before choosing theme for your "
-                                                 "urxvt-sensible-terminal", prog="theme-previewer")
-    parser.add_argument("-o, --output", help="Used to enable/disable printable output.\nLike which file we are "
+    parser = argparse.ArgumentParser(description="preview Xresource theme files before choosing theme for your "
+                                                 "urxvt-sensible-terminal")
+    parser.add_argument("-o, --output", help="used to enable/disable printable output. like which file we are "
                                              "looking at or which folder you wrote for input.", action='store_true')
-    parser.add_argument("-v, --version", help="Prints the version of this program!", action='store_true')
+    parser.add_argument("-v, --version", action="version", version="%(prog)s 2.0")
+    parser.add_argument("folder", help="the location of the folder containing any number of xresource files greater "
+                                       "than 0")
+    parser.add_argument("-s, --start", default="0", metavar="number", help="a number that tells which of the n number "
+                                                                           "of files to start from.")
     parser.print_help()
 
 
 def main():
     try:
-        opts, argv = getopt.getopt(sys.argv[1:], "hov", ["help", "output", "version"])
+        opts, argv = getopt.getopt(sys.argv[1:], "hovs:", ["help", "output", "version", "start="])
     except getopt.GetoptError as error:
         print(error.__str__())
         usage()
-        exit(2)
+        sys.exit(2)
 
     output = None
     themes_location = None
+    start = 0
 
     for o, a in opts:
         if o in ("-h", "--help"):
             usage()
-            exit()
+            sys.exit()
         elif o in ("-o", "--output"):
             output = 1
         elif o in ("-v", "--version"):
             print("ThemePreviewer 1.0")
+            sys.exit()
+        elif o in ("-s", "--start"):
+            start = int(a)
         else:
             usage()
             assert False, "There is no option like that."
 
     if not argv:
         usage()
+        print("")
         print("There must be some arguments!")
-        exit()
+        sys.exit()
 
     themes_location = argv[0]
 
@@ -84,17 +98,23 @@ def main():
         print("You must specify a folder where all the Xresources themes are located at.")
         exit()
 
+    clear()
     if not output:
         print("------------- Warning -------------")
         print("1. You have been warned about this ")
         print("2. There is no going back now!     ")
         print("3. There will be complete silence! ")
 
+    if output:
+        print("Location: " + themes_location)
+        print("Start at: " + str(start))
+
+    print("\033[1;" + str(int(rows) - 2) + "r")  # Change region temporarily by 2 rows while running the program
     preview(themes_location)
+    print("\033[1;" + rows + "r")  # Restores the region to the original position
 
 
 if __name__ == '__main__':
     rows, columns = os.popen("stty size", "r").read().split()
-    # print("\033[1;" + str(int(rows) - 2) + "r")  # Change region temporarily by 2 rows while running the program
     main()
-    # print("\033[1;" + rows + "r")  # Restores the region to the original position
+    sys.exit()
